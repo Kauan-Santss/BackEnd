8939205430b43fafcd803e62289e83c0dceaa7c0
Kauan Henrique <kauan_162@hotmail.com>
2021-02-23 11:29:01 -0300

    atualização

---
+++ Apache/web-backend/php-intro/form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="variaveis-pre-definidas.php" method="GET">
+    
+    <input type="text" name="nome" placeholder = "digite seu nome">
+
+    <input type="number" name="idade" placeholder="Digite sua idade">
+
+    <button>Enviar</button>
+    
+    </form>
+</body>
+</html>
